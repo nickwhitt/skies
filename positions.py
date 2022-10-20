23bd9cc17d09e7b5504809aa89194a388da410d7
Nick Whitt <nick.whitt@protonmail.com>
2022-10-20 15:15:24 -0400

    Caching

--- positions.py
+++ positions.py
@@ -1,24 +1,36 @@
 import json
+from datetime import datetime, timezone
 
+from pymemcache import serde
+from pymemcache.client.base import Client
 from skyfield.api import load
 
-t = load.timescale().now()
-jpl = load("de421.bsp")
+cache = Client("localhost", serde=serde.pickle_serde)
+
+t = load.timescale().from_datetime(
+    datetime.now(timezone.utc).replace(second=30, microsecond=0)
+)
 
 
 def distance_of(source, dest):
     return source.at(t).observe(dest).apparent().distance()
 
 
-response = [{"earth": {"sun": distance_of(jpl["earth"], jpl["sun"]).km}}]
-for p in ("mercury", "venus", "mars"):
-    response.append(
-        {
-            p: {
-                "earth": distance_of(jpl["earth"], jpl[p]).km,
-                "sun": distance_of(jpl[p], jpl["sun"]).km,
-            }
-        }
-    )
+response: list[dict] = cache.get(t.utc_iso())
+if response is None:
+    jpl = load("de421.bsp")
 
-print(json.dumps(response))
+    response = [{"earth": {"sun": distance_of(jpl["earth"], jpl["sun"]).km}}]
+    for p in ("mercury", "venus", "mars"):
+        response.append(
+            {
+                p: {
+                    "earth": distance_of(jpl["earth"], jpl[p]).km,
+                    "sun": distance_of(jpl[p], jpl["sun"]).km,
+                }
+            }
+        )
+
+    cache.set(t.utc_iso(), response, expire=60)
+
+print(t.utc_iso(), json.dumps(response))
